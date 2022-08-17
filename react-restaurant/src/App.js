60b4ef1028e9d02d52127c392a38f1b2514984ec
Rojalini Das <rojalini08das@gmail.com>
2022-08-18 00:53:20 +0530

    done

---
+++ react-restaurant/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RestauentDetails from "./components/RestaurentDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <RestauentDetails />
+    </div>
+  );
+}
+
+export default App;
