cb3e93c47af0cab75fda51e93c2eef193804dd1e
Rojalini Das <rojalini08das@gmail.com>
2022-09-09 10:27:59 +0530

    done

---
+++ React-Calculator/store.js
@@ -0,0 +1,13 @@
+class Store {
+  constructor(reducer, state) {
+    this.reducer = reducer;
+    this.state = state;
+  }
+  getState() {
+    return this.state;
+  }
+  dispatch(action) {
+    return this.reducer(this.state, action);
+  }
+}
+export default Store;
