cd680fcac5897151af521acb7a1621812fab6f3c
Rojalini Das <rojalini08das@gmail.com>
2022-10-11 10:22:53 +0530

    credit-card-pin

---
+++ Credit Card PIN/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Pin from "./component/Pin";
+import { useState } from "react";
+
+function App() {
+  const [otp, setOtp] = useState("");
+
+  return (
+    <div className="App">
+      <Pin length={4} setOtpHandler={(value) => setOtp(value)} />
+      <h3>OTP is {otp}</h3>
+    </div>
+  );
+}
+
+export default App;
